76f21bd46a6f0896efe7e9481de1e1c9a0191406
THUILLIERColin <90446137+THUILLIERColin@users.noreply.github.com>
2022-10-25 16:06:37 +0200

    Base du projet

---
+++ cocktails.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="fr">
+    <head>
+        <meta charset="utf-8" />
+        <title>GestionDeCocktails</title>
+    </head>
+
+    <body>
+        <header>
+            <h1><center>Les Cocktails </center></h1>
+        </header>
+        <nav border="1" style="width:100%">
+        <h2><center>Aliment courant</center></h2>
+        </nav>
+        <main border="1" style="width:100%">
+        <h2>Liste des cocktails</h2>
+    
+        <br/>
+        <?php
+       
+            
+        ?>
+    </main>
+        
+    <footer>
+    </footer>
+    </body>
+</html>
