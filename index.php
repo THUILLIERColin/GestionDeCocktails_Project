ea7f5497e0c714db7b977f71c3aa0f85a81cedba
THUILLIERColin <90446137+THUILLIERColin@users.noreply.github.com>
2022-11-04 23:13:43 +0100

    Ajout de la nav

---
+++ index.php
@@ -0,0 +1,23 @@
+<?php session_start() ?>
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>index</title>
+	<meta charset="utf-8" />
+    <link rel="stylesheet"  href="style.css" type="text/css"  media="screen" />
+</head>
+
+<body>
+
+<header>
+    <h1>Navigation</h1>
+</header>
+
+<nav>
+    <?php include("recette.php"); ?>
+</nav>
+
+<main>
+    
+</main>
--- recette.php
+++ recette.php
@@ -1,10 +1,42 @@
-<?php 
-    include("donnees.inc.php");
-    
-    foreach($hierarchie as $categorie => $ssCategorie)
-    {
-        echo 'categorie = '. $categorie ."\n";
-        echo 'sousCategorie = '. $ssCategorie[0] ."\n";
+<?php include("donnees.inc.php");?>
+<?php
+    $categorieActuelle = $_GET['categorie'];
 
-     }
-?>
+    if(!isset($_GET['categorie'])){
+        $_GET['categorie']='Aliment';
+        $_SESSION['historique'] = array( 0 => 'Aliment');
+    }
+    else {
+        if(in_array($categorieActuelle, $_SESSION['historique'])){
+            // indiceHistorique est la variable int de l'indice de la categorieActuelle dans historique
+            $indiceHistorique = array_search($_GET['categorie'], $_SESSION['historique']);
+            if($categorieActuelle='Aliment') array_splice($_SESSION['historique'], $indiceHistorique+1);
+            else array_splice($_SESSION['historique'], $indiceHistorique);
+        }
+        else {
+            if($categorieActuelle!='Aliment') array_push($_SESSION['historique'],$categorieActuelle);
+        }
+    }
+    ?>
+
+    <h1>Aliment courant</h1>
+    <?php 
+    foreach($_SESSION['historique'] as $precedent){
+        ?>
+        <a href="?categorie=<?php echo $precedent ?>"> <?php echo $precedent ?> </a> /
+        <?php
+    }
+    ?>
+    <ul>
+    <?php 
+    if(isset($hierarchie[end($_SESSION['historique'])]['sous-categorie'])){
+        ?>
+        <p>Sous - catégorie :</p>
+        <?php 
+        foreach($hierarchie[end($_SESSION['historique'])]['sous-categorie'] as $ingredient){
+            ?>
+            <li><a href="?categorie=<?php echo $ingredient ?>"> <?php echo $ingredient ?> </a></li>
+            <?php
+        }
+    }
+?></ul>
